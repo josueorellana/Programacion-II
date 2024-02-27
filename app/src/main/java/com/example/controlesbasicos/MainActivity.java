7c0bfe7e17cef61ce94e283ba921a2d4eee0dde7
josueorellana <jm5482017.com>
2024-02-27 11:52:55 -0300

    Cambios

--- app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -9,11 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     TabHost tbh;
+     private EditText txtAguaPotable, txtConversorCantidad;
+    Spinner spn;
+    Button btnAguaPotable, btnConversorConvertir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
         tbh.addTab(tbh.newTabSpec("Consumo de agua potable").setContent(R.id.tabAguaPotable).setIndicator("Consumo de agua potable", null));
         tbh.addTab(tbh.newTabSpec("Conversor de area").setContent(R.id.tabConversor).setIndicator("Conversor de area", null));
     }
-}
+    class Superficie{
+        double[][] valores = {
+                {1,100, 39.3701, 3.280841666667, 1.193, 1.0936138888889999077,
+                        0.001, 0.000621371}
+        };
+        public double convertir(int opcion, int de, int a, double cantidad){
+            return valores[opcion][a] / valores[opcion][de] * cantidad;
+        }
+
+    }
+    }
 
