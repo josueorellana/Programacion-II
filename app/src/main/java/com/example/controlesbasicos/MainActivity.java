a6517df5580337734a9fcce548103c491ed5ab63
josueorellana <jm5482017.com>
2024-02-27 17:50:54 -0300

    codigo

--- app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -2,22 +2,20 @@ package com.example.controlesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.text.InputFilter;
-import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
-import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     TabHost tbh;
-     private EditText txtAguaPotable, txtConversorCantidad;
+    private EditText txtAguaPotable, txtConversorCantidad;
     Spinner spn;
-    Button btnAguaPotable, btnConversorConvertir;
+    Button btnAguaPotable, btnConvertir;
+
+    Superficie ConversorArea = new Superficie();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +26,40 @@ public class MainActivity extends AppCompatActivity {
 
         tbh.addTab(tbh.newTabSpec("Consumo de agua potable").setContent(R.id.tabAguaPotable).setIndicator("Consumo de agua potable", null));
         tbh.addTab(tbh.newTabSpec("Conversor de area").setContent(R.id.tabConversor).setIndicator("Conversor de area", null));
+
+        btnConvertir = findViewById(R.id.btnConvertir);
+        btnAguaPotable = findViewById(R.id.btnAguaPotable);
+        btnConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spn = findViewById(R.id.spnConversorDe);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnConversorA);
+                int a = spn.getSelectedItemPosition();
+
+                txtConversorCantidad = findViewById(R.id.txtConversorCantidad);
+                double cantidad = Double.parseDouble(txtConversorCantidad.getText().toString());
+
+                double resp = ConversorArea.convertir(0, de, a, cantidad);
+                Toast.makeText(getApplicationContext(),"Respuesta: "+
+                        resp, Toast.LENGTH_LONG).show();
+            }
+        });
     }
-    class Superficie{
-        double[][] valores = {
-                {1,100, 39.3701, 3.280841666667, 1.193, 1.0936138888889999077,
-                        0.001, 0.000621371}
-        };
-        public double convertir(int opcion, int de, int a, double cantidad){
-            return valores[opcion][a] / valores[opcion][de] * cantidad;
+}
+
+        class Superficie {
+            double[][] valores = {
+                    {1, 629.856, 0.0929, 0.835905, 0.836, 6988.96, 10000}
+            };
+
+            public double convertir(int opcion, int de, int a, double cantidad) {
+                return valores[opcion][a] / valores[opcion][de] * cantidad;
+            }
+
+
         }
 
-    }
-    }
+
 
