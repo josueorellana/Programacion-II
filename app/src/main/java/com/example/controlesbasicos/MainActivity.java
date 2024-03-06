a2fd8d9704d1f4d82b1f30032e846cf674251d7a
josueorellana <jm5482017.com>
2024-03-06 10:55:13 -0300

    Diseño

--- app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.controlesbasicos;
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
     }
 }
